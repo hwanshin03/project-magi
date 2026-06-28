6809b38e1f9bf78b68a09f499ac7a5682407ed9f
Hwan Shin <tlsghks0309@gmail.com>
2026-06-27 21:49:06 -0700

    Initial commit: Project MAGI started

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Project MAGI started")
+
+melchior = "Open AI"
+balthasar = "Gemini"
+casper = "Claude"
+
+print("Melchior:" , melchior)
+print("Balthasar:" , balthasar)
+print("Casper:" , casper)
+
+print("Consensus Engine: Waiting for signal...")
