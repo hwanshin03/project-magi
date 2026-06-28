956c4b13826c223871fa0bcdc7b34e854b649b7d
Hwan Shin <tlsghks0309@gmail.com>
2026-06-28 01:35:53 -0700

    Connect Balthasar to Gemini API

--- magi/balthasar.py
+++ magi/balthasar.py
@@ -1,6 +1,21 @@
+import os
+
+from dotenv import load_dotenv
+from google import genai
+
 class Balthasar:
     def __init__(self):
+        load_dotenv()
+
         self.name = "Balthasar"
+        self.client = genai.Client(
+            api_key=os.getenv("GEMINI_API_KEY")
+        )
 
     def think(self, question):
-        return f"[{self.name}] thinks: {question}"
+        response = self.client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=question
+        )
+       
+        return f"[{self.name}]\n{response.text}"
--- magi/melchior.py
+++ magi/melchior.py
@@ -1,7 +1,22 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+
 class Melchior:
     def __init__(self):
         self.name = "Melchior"
-    
-    def think(self, question) :
-        return f"[{self.name}] thinks: {question}"
-    
+
+        self.client = OpenAI(
+            api_key=os.getenv("OPENAI_API_KEY")
+        )
+
+    def think(self, question):
+        response = self.client.responses.create(
+            model="gpt-5.5",
+            input=question
+        )
+
+        return f"[{self.name}]\n{response.output_text}"
